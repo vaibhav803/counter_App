474ae8dfbed4aef31b241d7cfaa537a843d6bc60
vaibhav803 <156533388+vaibhav803@users.noreply.github.com>
2025-04-24 16:27:50 +0530

    Update index.js
    saveel text update

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function increment() {
 
 function save() {
     let countStr = count + " - "
-    saveEl.textContenttStr
+    saveEl.textContenttStr = countStr
     countEl.textContent = 0
     count = 0
 }
