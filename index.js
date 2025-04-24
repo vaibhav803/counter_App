66f6bf7d01e4223049fe331c974f9066288f3e74
vaibhav803 <156533388+vaibhav803@users.noreply.github.com>
2025-04-24 16:32:26 +0530

    Update index.js
    update problem 1

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function increment() {
 
 function save() {
     let countStr = count + " - "
-    saveEl.textContent = countStr
+    saveEl.textContent += countStr
     countEl.textContent = 0
     count = 0
 }
